4274d98bc8719d1e3d12ac0450fa92fedf958d94
Tyler <Tyl3r.s@outlook.com>
2022-10-04 19:26:11 -0400

    edit post model & api routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Comment, Vote } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// get all users
+// get all posts
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
     attributes: [
       'id',
       'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'text',
+      'created_at'
     ],
     include: [
       {
@@ -77,10 +76,10 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  // create post
   Post.create({
     title: req.body.title,
-    post_url: req.body.post_url,
+    text: req.body.text,
     user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
@@ -90,20 +89,13 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.put('/upvote', withAuth, (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// Edit a post
 
 router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
-      title: req.body.title
+      title: req.body.title,
+      text: req.body.text     
     },
     {
       where: {
@@ -123,6 +115,8 @@ router.put('/:id', withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// Delete a post
 
 router.delete('/:id', withAuth, (req, res) => {
   console.log('id', req.params.id);
